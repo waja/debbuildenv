d8876fc31312f11fe6155d0b4976ef932a11171e
Jan Wagner <waja@cyconet.org>
2023-12-04 14:18:30 +0100

    Fix entrypoint for archived distibutions

--- generic/local/bin/entrypoint.sh
+++ generic/local/bin/entrypoint.sh
@@ -3,12 +3,19 @@
 if [ ! -f /.initialized ]; then
 	[ -n "${CACHE_HOST}" ] && echo "Acquire::http::Proxy \"http://${CACHE_HOST}:3142\";" > /etc/apt/apt.conf.d/01proxy; \
 	printf "APT::Install-Recommends \"false\";\nAptitude::Recommends-Important \"False\";" >	/etc/apt/apt.conf.d/00InstallRecommends && \
-	[ -n "${LOCAL_DEB_MIRROR}" ] && [ -f /etc/apt/sources.list ] && sed -i "s#http://deb.debian.org/debian #${LOCAL_DEB_MIRROR}/debian #g" /etc/apt/sources.list;  \
+	[ -n "${LOCAL_DEB_MIRROR}" ] && [ -f /etc/apt/sources.list ] && \
+		if [ "${LOCAL_DEB_MIRROR}" == "http://archive.debian.org" ]; then
+			sed -i "s#http://deb.debian.org/debian #[check-valid-until=no] ${LOCAL_DEB_MIRROR}/debian #g" /etc/apt/sources.list && sed -i '/security.debian.org/d' /etc/apt/sources.list && sed -i '/-updates/d' /etc/apt/sources.list 
+			APT_FORCE_YES="--force-yes"
+			CURL_INSECURE="-k"
+		else
+			sed -i "s#http://deb.debian.org/debian #${LOCAL_DEB_MIRROR}/debian #g" /etc/apt/sources.list
+		fi;  \
 	[ -n "${LOCAL_DEB_MIRROR}" ] && [ -f /etc/apt/sources.list.d/debian.sources ] && sed -i "s#http://deb.debian.org/debian\$#${LOCAL_DEB_MIRROR}/debian#g" /etc/apt/sources.list.d/debian.sources;  \
 	apt-get update > /dev/null && apt-get -y upgrade > /dev/null && \
-	apt-get install -y --no-install-recommends curl ca-certificates gnupg adduser > /dev/null && \
+	apt-get install -y "${APT_FORCE_YES:-}" --no-install-recommends curl ca-certificates gnupg adduser > /dev/null && \
 	[ "${BUILD_USER}" != "root" ] && adduser -shell /bin/bash --gecos '' --disabled-password --home "${USER_HOME_DIR}" "${BUILD_USER}" > /dev/null && sed -i "s/# auth       sufficient pam_wheel.so trust/auth       sufficient pam_wheel.so trust group=${BUILD_USER}/" /etc/pam.d/su; \
-	curl -fsSL https://ftp.cyconet.org/debian/repo.asc | gpg --dearmor | tee /etc/apt/trusted.gpg.d/debian-cyconet-archive-keyring.gpg > /dev/null 2>&1 && \
+	curl -fsSL "${CURL_INSECURE:-}" https://ftp.cyconet.org/debian/repo.asc | gpg --dearmor | tee /etc/apt/trusted.gpg.d/debian-cyconet-archive-keyring.gpg > /dev/null 2>&1 && \
 	cat << EOF > /etc/apt/sources.list.d/restricted-cyconet.list
 deb     [signed-by=/etc/apt/trusted.gpg.d/debian-cyconet-archive-keyring.gpg] http://ftp.cyconet.org/debian restricted     main non-free contrib
 deb-src [signed-by=/etc/apt/trusted.gpg.d/debian-cyconet-archive-keyring.gpg] http://ftp.cyconet.org/debian restricted     main non-free contrib
